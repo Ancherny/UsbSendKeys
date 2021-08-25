2902d4623028d80467d43552f61d9fafcb8e6964
ancher <ancher@mail.ru>
2021-08-25 15:59:27 +0300

    Freerider test

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 
-namespace UsbSendKeys
+internal static class Program
 {
-    internal static class Program
+    private const string freeRiderProcessName = "FPVFreerider_Recharged";
+    private const uint keyDownMsg = 0x0100;
+    private const int keyU = 0x55;
+
+    [DllImport("user32.dll")]
+    private static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+    [STAThread]
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        Process [] processes = Process.GetProcessesByName(freeRiderProcessName);
+        while(true)
         {
-            Console.WriteLine("Hello World!");
+            foreach (Process proc in processes)
+            {
+                PostMessage(proc.MainWindowHandle, keyDownMsg, keyU, 0);
+            }
+            Thread.Sleep(5000);
         }
+        // ReSharper disable once FunctionNeverReturns
     }
 }
