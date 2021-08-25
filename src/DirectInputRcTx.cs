ad991493b76afe8362845edcda891708af68a1fa
ancher <ancher@mail.ru>
2021-08-25 21:58:06 +0300

    Get named device

--- src/DirectInputRcTx.cs
+++ src/DirectInputRcTx.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using SharpDX.DirectInput;
 
 public class DirectInputRcTx : IRcTx
 {
-    public static bool CreateByName(out IRcTx tx, string name)
+    private readonly DeviceInstance _txDevice;
+
+    private DirectInputRcTx(DeviceInstance txDevice)
     {
+        _txDevice = txDevice;
+    }
+
+    public static bool CreateByName(out IRcTx tx, string txName)
+    {
+        tx = null;
         DirectInput directInput = new DirectInput();
         IList<DeviceInstance> devices = directInput.GetDevices(DeviceClass.All, DeviceEnumerationFlags.AllDevices);
-        Log.Info("Huy!");
-        tx = new DirectInputRcTx();
+
+        DeviceInstance txDevice = devices.FirstOrDefault(device => device.InstanceName == txName);
+        if (txDevice == null)
+        {
+            Log.Error($"Cannot find tx device by name '{txName}'");
+            return false;
+        }
+
+        tx = new DirectInputRcTx(txDevice);
         return true;
     }
 }
