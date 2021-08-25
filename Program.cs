1d93d2f1e7ed14e02ae82f69b929bc41a996e54f
ancher <ancher@mail.ru>
2021-08-25 17:47:30 +0300

    Get process name from cmd

--- Program.cs
+++ Program.cs
@@ -5,25 +5,71 @@ using System.Threading;
 
 internal static class Program
 {
-    private const string freeRiderProcessName = "FPVFreerider_Recharged";
+    private const string procNameArg = "proc_name";
     private const uint keyDownMsg = 0x0100;
     private const int keyU = 0x55;
+
+    private static string procName = null;
 
     [DllImport("user32.dll")]
     private static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
 
-    [STAThread]
-    private static void Main(string[] args)
+    private static void GiveHelp()
     {
-        Process [] processes = Process.GetProcessesByName(freeRiderProcessName);
-        while(true)
+        Log.Info("Expected command line:");
+        Log.Info("-args:img=<process_name>");
+        Log.Info("  where process_name defines running process to send keystrokes to");
+    }
+
+    private static bool ParseArguments()
+    {
+        bool isSuccess = false;
+        do
         {
-            foreach (Process proc in processes)
+            if (!ArgsReader.GetString(out procName, procNameArg))
+            {
+                Log.Error("No process name defined.");
+                break;
+            }
+            
+            isSuccess = true;
+
+        } while (false);
+
+        return isSuccess;
+    }
+    
+    [STAThread]
+    private static void Main()
+    {
+        do
+        {
+            if (!ParseArguments())
+            {
+                GiveHelp();
+                break;
+            }
+            
+            Process [] processes = Process.GetProcessesByName(procName);
+            if (processes.Length <= 0)
+            {
+                Log.Error($"No running process '{procName}' found.");
+                break;
+            }
+
+            if (processes.Length != 1)
+            {
+                Log.Error($"More than one process '{procName}' is running.");
+                break;
+            }
+
+            Process proc = processes[0];
+            while(true)
             {
                 PostMessage(proc.MainWindowHandle, keyDownMsg, keyU, 0);
+                Thread.Sleep(5000);
             }
-            Thread.Sleep(5000);
-        }
-        // ReSharper disable once FunctionNeverReturns
+            
+        } while (false);
     }
 }
