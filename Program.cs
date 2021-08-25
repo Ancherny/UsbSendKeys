f076c26dba8f9ec3875c2b11433435d0e3339743
ancher <ancher@mail.ru>
2021-08-25 15:25:37 +0300

    Dummy solution added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsbSendKeys
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
