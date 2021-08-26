deb4809de5d658a271dd130a354b39a63a031df1
ancher <ancher@mail.ru>
2021-08-26 07:41:04 +0300

    Getting joystick state

--- src/DirectInputRcTx.cs
+++ src/DirectInputRcTx.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using SharpDX.DirectInput;
 
 public class DirectInputRcTx : IRcTx
 {
     private readonly Joystick _txJoystick;
+    private readonly int[] _channelsState = new int[8];
 
     private DirectInputRcTx(Joystick txJoystick)
     {
@@ -28,30 +29,47 @@ public class DirectInputRcTx : IRcTx
         Joystick txJoystick = new Joystick(directInput, txDevice.InstanceGuid);
         tx = new DirectInputRcTx(txJoystick);
 
-        foreach (DeviceObjectInstance doi in txJoystick.GetObjects(DeviceObjectTypeFlags.AbsoluteAxis))
-        {
-            Log.Info(doi.Name);
-            Log.Info(doi.ObjectId.ToString());
-        }
-
-        // Set BufferSize in order to use buffered data.
-        txJoystick.Properties.BufferSize = 128;
-
         // Acquire the joystick
-        txJoystick.Acquire();
-
-        // Poll events from joystick
-        while (true)
+        try
         {
-            txJoystick.Poll();
-            JoystickState state = txJoystick.GetCurrentState();
-            JoystickUpdate[] joystickUpdates = txJoystick.GetBufferedData();
-            foreach (JoystickUpdate joystickUpdate in joystickUpdates)
-            {
-                Log.Info(joystickUpdate.ToString());
-            }
-            Thread.Sleep(1000);
+            txJoystick.Acquire();
         }
+        catch (Exception e)
+        {
+            Log.Error(e.ToString());
+            Log.Error("Failed to acquire joystick device.");
+            return false;
+        }
+
         return true;
+    }
+
+    private static int AxisValueToMicroseconds(int axisValue)
+    {
+        return (int)(1.0f / uint.MaxValue * axisValue * 1000 + 1000);
+    }
+
+    public bool GetChannelsState(out int[] channelsState)
+    {
+        JoystickState state = _txJoystick.GetCurrentState();
+        _channelsState[0] = AxisValueToMicroseconds(state.X);
+        _channelsState[1] = AxisValueToMicroseconds(state.Y);
+        _channelsState[2] = AxisValueToMicroseconds(state.Z);
+        _channelsState[3] = AxisValueToMicroseconds(state.RotationX);
+        _channelsState[4] = AxisValueToMicroseconds(state.RotationY);
+        _channelsState[5] = AxisValueToMicroseconds(state.RotationZ);
+        _channelsState[6] = AxisValueToMicroseconds(state.Sliders[0]);
+        _channelsState[7] = AxisValueToMicroseconds(state.Sliders[1]);
+        channelsState = _channelsState;
+        return true;
+    }
+
+    public void Dispose()
+    {
+        if (_txJoystick != null)
+        {
+            _txJoystick.Unacquire();
+            _txJoystick.Dispose();
+        }
     }
 }
--- src/IRcTx.cs
+++ src/IRcTx.cs
@@ -1,3 +1,8 @@
-﻿public interface IRcTx
+﻿using System;
+
+public interface IRcTx : IDisposable
 {
+    // Polls current channels' state.
+    // Channel state is standard RC TX microseconds range of (1000:2000) 
+    bool GetChannelsState(out int[] state);
 }
